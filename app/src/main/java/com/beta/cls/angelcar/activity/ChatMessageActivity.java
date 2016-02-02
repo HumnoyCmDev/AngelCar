34a19197ed16772deed879711fc6a091e65447ee
humnoy <humnoy.cmdev@gmail.com>
2016-02-02 08:30:40 +0700

    update 02/02/2016 #1

--- app/src/main/java/com/beta/cls/angelcar/activity/ChatMessageActivity.java
+++ app/src/main/java/com/beta/cls/angelcar/activity/ChatMessageActivity.java
@@ -63,7 +63,7 @@ public class ChatMessageActivity extends AppCompatActivity{
         }
         loadMessage();
 
-        // test up mac 2
+        // test up mac 02/02/2016
     }
 
     private void loadMessage() {
